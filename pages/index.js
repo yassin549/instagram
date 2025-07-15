1df50a681e98f0f0491df8e72b158bae94596686
yassin549 <khoualdiyassin26@gmail.com>
2025-07-15 06:09:51 +0100

    commit

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-black text-white font-sans">
+      <h1 className="text-4xl font-bold">Welcome to the Liquid-Glass E-Commerce Platform</h1>
+    </div>
+  );
+}
